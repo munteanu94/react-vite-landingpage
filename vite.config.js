04a1823a0042d776e17688d437cd312068b86d19
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2024-01-26 15:28:31 +0200

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-vite-landingpage/dist/",
+  base: "/react-vite-landingpage/",
   plugins: [react()],
 });
